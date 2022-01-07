a2370b21bcfab8657d6eb176b8b62bc79d42cd3a
Ahmed Kamran <akamran@colgate.edu>
2022-01-07 04:03:51 -0500

    board working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import "./App.css";
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      player: "x",
+    };
+  }
+  colorBoard() {
+    const board = document.getElementById("board");
+    const rows = Array.from(board.rows);
+    const colorMap = [
+      ["red", "green", "blue"],
+      ["green", "blue", "red"],
+      ["blue", "red", "green"],
+    ];
+    rows.map((tr, i) => {
+      const cols = Array.from(tr.cells);
+      cols.map((cell, j) => {
+        cell.className = colorMap[i][j];
+        return null;
+      });
+      return null;
+    });
+  }
+  play(cell, player) {
+    const clicked = cell.getAttribute("clicked");
+    if (clicked === "false") {
+      cell.setAttribute("clicked", "true");
+      cell.innerHTML = `<h1>${player}</h1>`;
+      const next = player === "x" ? "o" : "x";
+      this.setState({ player: next });
+    }
+  }
+  cellAssign(player) {
+    const cells = Array.from(document.getElementsByTagName("td"));
+    cells.map((cell, index) => {
+      cell.id = String(index);
+      cell.setAttribute("clicked", "false");
+      cell.addEventListener("click", (e) => {
+        this.play(e.target, player);
+      });
+    });
+    return null;
+  }
+  componentDidMount() {
+    this.colorBoard();
+    this.cellAssign(this.state.player);
+  }
+  componentDidUpdate() {
+    this.cellAssign(this.state.player);
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Tic Tac Toe</h1>
+        <table id="board">
+          <tbody>
+            <tr>
+              <td className="cell"></td>
+              <td className="cell"></td>
+              <td className="cell"></td>
+            </tr>
+            <tr>
+              <td className="cell"></td>
+              <td className="cell"></td>
+              <td className="cell"></td>
+            </tr>
+            <tr>
+              <td className="cell"></td>
+              <td className="cell"></td>
+              <td className="cell"></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
-
-export default App;
