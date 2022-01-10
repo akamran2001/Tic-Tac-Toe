868b3d45d3655d64819a0ea6d3cd2f216e8266ce
Ahmed Kamran <akamran@colgate.edu>
2022-01-10 15:37:19 -0500

    Updates

---
+++ precache-manifest.2493e186892254c475db4407bb510e59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7cf0ba60dda382e6b9fe1bb0e4b1616",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "36346f9ecb65cae4bcb4",
+    "url": "/Tic-Tac-Toe/static/css/main.c6e17018.chunk.css"
+  },
+  {
+    "revision": "c774690aec95a981bdbf",
+    "url": "/Tic-Tac-Toe/static/js/2.e440e22d.chunk.js"
+  },
+  {
+    "revision": "7f629e7136e2105b6ae2",
+    "url": "/Tic-Tac-Toe/static/js/3.432105df.chunk.js"
+  },
+  {
+    "revision": "36346f9ecb65cae4bcb4",
+    "url": "/Tic-Tac-Toe/static/js/main.c2109159.chunk.js"
+  },
+  {
+    "revision": "b1c8406e9f1aaeabc271",
+    "url": "/Tic-Tac-Toe/static/js/runtime~main.4ab7ad24.js"
+  }
+]);
